6fe3e89f8d2a3d3713dc4a6aca37d1d4430a1c52
Moonkey <moonkey48@icloud.com>
2023-03-16 18:03:13 +0900

    [Basic1] add landmark list

---
+++ 1-tutorial/1-tutorial/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  1-tutorial
+//
+//  Created by Seungui Moon on 2023/03/16.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
---
+++ 1-tutorial/1-tutorial/Views/LandmarkRow.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkRow.swift
+//  1-tutorial
+//
+//  Created by Seungui Moon on 2023/03/16.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+
+            Spacer()
+        }
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow(landmark: landmarks[1])
+            .previewLayout(.fixed(width: 300, height: 70))
+            
+    }
+}
