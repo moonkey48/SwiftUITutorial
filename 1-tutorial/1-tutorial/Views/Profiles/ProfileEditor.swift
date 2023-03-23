be2f856cf00ffcc73c1c1f262eb6a33faa8732a2
Moonkey <moonkey48@icloud.com>
2023-03-23 18:53:12 +0900

    [Profile] add Profile edit mode

---
+++ 1-tutorial/1-tutorial/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,52 @@
+//
+//  ProfileEditor.swift
+//  1-tutorial
+//
+//  Created by Seungui Moon on 2023/03/23.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    @Binding var profile: Profile
+    
+    var dateRange: ClosedRange<Date> {
+        let min = Calendar.current.date(byAdding: .year, value: -1, to: profile.goalData)!
+        let max = Calendar.current.date(byAdding: .year, value: 1, to: profile.goalData)!
+        return min...max
+    }
+    
+    var body: some View {
+        List {
+            HStack {
+                Text("User Name").bold()
+                Divider()
+                TextField("Username", text: $profile.userName)
+            }
+            Toggle(isOn: $profile.preferNotifications) {
+                Text("Enable Notifications").bold()
+            }
+            
+            VStack(alignment: .leading) {
+                Text("Seasonal Photo").bold()
+                
+                Picker("Seasonal Photo", selection: $profile.seasonalPhoto) {
+                    ForEach(Profile.Season.allCases) { season in
+                        Text(season.rawValue).tag(season)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            DatePicker(selection: $profile.goalData, in: dateRange, displayedComponents: .date) {
+                Text("Goal Date").bold()
+            }
+        }
+    }
+}
+
+struct ProfileEditor_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileEditor(profile: .constant(.default))
+    }
+}
--- 1-tutorial/1-tutorial/Views/Profiles/ProfileHost.swift
+++ 1-tutorial/1-tutorial/Views/Profiles/ProfileHost.swift
@@ -8,11 +8,23 @@
 import SwiftUI
 
 struct ProfileHost: View {
+    @Environment(\.editMode) var editMode
+    @EnvironmentObject var modelData: ModelData
     @State var draftProfile = Profile.default
     
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
-            ProfileSummary(profile: Profile.default)
+            
+            HStack {
+                Spacer()
+                EditButton()
+            }
+            
+            if editMode?.wrappedValue == .inactive {
+                ProfileSummary(profile: modelData.profile)
+            } else {
+                ProfileEditor(profile: $draftProfile)
+            }
         }
         .padding()
     }
